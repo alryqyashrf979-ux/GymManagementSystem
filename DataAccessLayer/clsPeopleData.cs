6b8c28232affb730fae22c17e8fb2de1d838149d
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-09 18:10:45 +0300

    Get All People

---
+++ DataAccessLayer/clsPeopleData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAccessLayer
+{
+   static public class clsPeopleData
+    {
+
+        static public DataTable GetAllPeople()
+        {
+            DataTable dt = new DataTable();
+
+            SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString);
+            string query = "select *from People;";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+
+            }
+            catch
+            {
+              
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+
+    }
+}
