6a9a9535ae112a0291a41078703659ac1291b599
Aiman Ameen <aymnamynalamry@gmail.com>
2026-09-09 23:48:21 +0200

    Add class management logic to ClassesDataBusiness
    Added new properties to represent class details, Added `AddClass()` for adding new classes and `Save()` to handle saving logic based on the current mode, supporting both adding and updating classes.

--- DataBusinessLayer/clsClassesDataBusiness.cs
+++ DataBusinessLayer/clsClassesDataBusiness.cs
@@ -10,6 +10,66 @@ namespace DataBusinessLayer
     public class ClassesDataBusiness
     {
 
+        public int ClassID { get; set; }
+        public string className { get; set; }
+        public string Description { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Note { get; set; }
+        public byte Maximum_Capacity { get; set; }
+        public bool IsActive { get; set; }
+        public string StartDay { get; set; }
+        public string EndDay { get; set; }
+        public int CoachID { get; set; }
+
+
+        private enum enMode { AddNew = 1, Update = 2 }
+        enMode _Mode = enMode.AddNew;
+
+
+
+        //  Implemented a private `AddClass()` method to handle adding new
+        //classes via
+        private bool AddClass()
+        {
+            this.ClassID = DataAccessLayer.ClassesDataAccess.AddNewClass(this.CoachID, this.className, this.Description, this.StartTime,
+                this.EndTime, this.Note,
+                this.Maximum_Capacity, this.IsActive, this.StartDay, this.EndDay);
+
+            return (this.ClassID != -1);
+        }
+
+
+        //Added a public `Save()` method to manage saving logic based on
+        //the current mode, supporting both adding and updating classes.
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+
+                case enMode.AddNew:
+                    if (AddClass())
+                    {
+                        _Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return true;
+
+            }
+
+            return false;
+        }
+
+
+
+
+
 
 
 
