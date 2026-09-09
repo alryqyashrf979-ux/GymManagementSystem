e4b1bf5e729d39a1ac21ded28bc59a53b27ebb14
Aiman Ameen <aymnamynalamry@gmail.com>
2026-09-09 22:42:16 +0200

    Added UpdateClass Function

--- DataAccessLayer/ClassesDataAccess.cs
+++ DataAccessLayer/ClassesDataAccess.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -104,6 +106,79 @@ namespace DataAccessLayer
 
             return ClassID;
 
+        }
+
+
+
+        public static bool UpdateClass(int ClassID, string className, string Description, TimeSpan StartTime, TimeSpan EndTime, string Note, byte Maximum_Capacity,
+       bool IsActive, string StartDay, string EndDay)
+        {
+
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString);
+
+            string query = @"UPDATE [dbo].[Classes]
+        SET [className] = @className
+      ,[Description] = @Description
+      ,[StartTime] = @StartTime
+      ,[EndTime] = @EndTime
+      ,[Note] = @Note
+      ,[Maximum_Capacity] = @Maximum_Capacity
+      ,[IsActive] = @IsActive
+      ,[EndDay] = @EndDay
+      ,[StartDay] = @StartDay
+          WHERE ClassID =@ClassID";
+
+            SqlCommand Command = new SqlCommand(query, connection);
+
+            Command.Parameters.AddWithValue("@ClassID", ClassID);
+            Command.Parameters.AddWithValue("@className", className);
+
+
+            if (Description != "" && Description != null)
+                Command.Parameters.AddWithValue("@Description", Description);
+            else
+                Command.Parameters.AddWithValue("@Description", DBNull.Value);
+
+            Command.Parameters.AddWithValue("@StartTime", StartTime);
+            Command.Parameters.AddWithValue("@EndTime", EndTime);
+
+            if (Note != "" && Note != null)
+                Command.Parameters.AddWithValue("@Note", Note);
+            else
+                Command.Parameters.AddWithValue("@Note", DBNull.Value);
+
+            Command.Parameters.AddWithValue("@Maximum_Capacity", Maximum_Capacity);
+            Command.Parameters.AddWithValue("@IsActive", IsActive);
+
+            if (EndDay != "" && EndDay != null)
+                Command.Parameters.AddWithValue("@EndDay", EndDay);
+            else
+                Command.Parameters.AddWithValue("@EndDay", DBNull.Value);
+
+            if (StartDay != null && StartDay != null)
+                Command.Parameters.AddWithValue("@StartDay", StartDay);
+            else
+                Command.Parameters.AddWithValue("@StartDay", DBNull.Value);
+
+
+            try
+            {
+                connection.Open();
+                rowsAffected = Command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
         }
 
 
