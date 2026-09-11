b73f6dec90f799135eaf5b3387d4cbe9c82c6d04
Ashraf <alryqyashrf979@gmail.com>
2026-09-11 18:19:16 +0300

    added all subscription plan class properties and constructors

--- DataBusinessLayer/clsSubscriptionPlans.cs
+++ DataBusinessLayer/clsSubscriptionPlans.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace DataBusinessLayer
 {
-    public class Class1
+    public class clsSubscriptionPlans
     {
 
+        //PlanID , PlanName , PlanDescription , Availablity, PlanPrice  , Note .
+        private int _PlanID = -1;
+        public int PlanID { get { return _PlanID; } }
 
+        public string PlanName { set; get; }
+        public string PlanDescription { set; get; }
+        public bool Availiability { set; get; }
+
+        public decimal PlanPrice { set; get; }
+
+        public string Note { set; get; }
+
+        enum enMode { Add, Update };
+        enMode Mode = enMode.Add;
+
+        public clsSubscriptionPlans()
+        {
+            _PlanID = -1;
+            PlanName = string.Empty;
+            PlanDescription = string.Empty;
+            Availiability = true;
+            PlanPrice = default(decimal);
+            Note = string.Empty;
+            Mode = enMode.Add;
+
+        }
+        public clsSubscriptionPlans(int planID, string planName, string planDescription, bool availiablity, decimal planPrice, string note)
+        {
+            this._PlanID = planID;
+            this.PlanName = planName;
+            this.PlanDescription = planDescription;
+            this.Availiability = availiablity;
+            this.PlanPrice = planPrice;
+            this.Note = note;
+            this.Mode = enMode.Update;
+
+        }
 
 
 
