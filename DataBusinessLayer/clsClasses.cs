e1cd90788222be51169e0a712a597d99d4e5eb13
Aiman Ameen Mohammed <aymnamynalamry@gmail.com>
2026-09-11 16:03:28 +0300

    Adding cls classes /Data Business Layer/Aiman (#7)
    * Add ClassesDataAccess
    
    * Added AddNewClass Function
    
    * Added UpdateClass Function
    
    * Add CoachID support to class management methods
    
    Updated the `AddNewClass` and `UpdateClass` methods
    
    * Add GetAllClasses method for retrieving class data
    
    * Add DeleteClass Method
    
    * Add GetClassInfoByClassID method to ClassesDataAccess
    
    * Add clsClassesDataBusiness.cs to DataBusinessLayer
    
    * Add class management logic to ClassesDataBusiness
    
    Added new properties to represent class details, Added `AddClass()` for adding new classes and `Save()` to handle saving logic based on the current mode, supporting both adding and updating classes.
    
    * Refactor Save logic and add class update functionality
    
    add `_UpdateClass()` to handle updating classes, respectively. Modified the `Save()` method to use  `_UpdateClass()` for updating existing classes.
    
    * Add GetAllClasses method to clsClassesDataBusiness
    
    * Add DeleteClass method to clsClassesDataBusiness
    
    * Refactor clsClassesDataBusiness and add new methods
    
    Renamed `ClassesDataBusiness` to `clsClassesDataBusiness` .
    
    Added a private constructor to initialize all properties and a parameterless public constructor with default values.
    
    added  method GetClassInfoByClassID
    to clsClassesDataBusiness
    
    * Renamed `clsClassesDataBusiness` to `clsClasses` for simplification
    
    * Refactor ClassesDataAccess to clsClassesData
    
    Renamed `ClassesDataAccess` to `clsClassesData` across the project to align with updated naming conventions.
    
    * edited Class id's properties and methods
    
    ---------
    
    Co-authored-by: Ashraf <alryqyashrf979@gmail.com>

---
+++ DataBusinessLayer/clsClasses.cs
@@ -0,0 +1,167 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBusinessLayer
+{
+    public class clsClasses
+    {
+        // Ashraf edited the property and access modifier of the class ID
+        private int _ClassID = -1;
+        public int ClassID { get { return _ClassID; } }
+        public string className { get; set; }
+        public string Description { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Note { get; set; }
+        public byte Maximum_Capacity { get; set; }
+        public bool IsActive { get; set; }
+        public string StartDay { get; set; }
+        public string EndDay { get; set; }
+        public int CoachID { get; set; }
+
+
+        private enum enMode { AddNew = 1, Update = 2 }
+        enMode _Mode = enMode.AddNew;
+
+
+
+
+        private clsClasses(int classID, string className, string description, TimeSpan startTime, TimeSpan endTime,
+            string note, byte maximum_Capacity, bool isActive, string startDay, string endDay, int coachID)
+        {
+            // Ashraf edited this.ClassID to this._ClassID
+            this._ClassID = classID;
+            this.className = className;
+            this.Description = description;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            this.Note = note;
+            this.Maximum_Capacity = maximum_Capacity;
+            this.IsActive = isActive;
+            this.StartDay = startDay;
+            this.EndDay = endDay;
+            this.CoachID = coachID;
+
+            _Mode = enMode.Update;
+             
+        }
+
+
+        public clsClasses()
+        {
+            // Ashraf edited this.ClassID to this._ClassID
+            this._ClassID = -1;
+            this.CoachID = -1;
+            this.Note = "";
+            this.StartDay = "";
+            this.EndDay = "";
+            this.EndTime = TimeSpan.Zero;
+            this.StartTime= TimeSpan.Zero;
+            this.IsActive = false;
+            this.Description = "";
+            this.Maximum_Capacity = 0;
+            this.className = "";
+
+            _Mode = enMode.AddNew;
+
+        }
+
+
+
+
+        //  Implemented a private `AddClass()` method to handle adding new
+        //classes via
+        private bool _AddClass()
+        {
+            // Ashraf edited this.ClassID to this._ClassID
+            this._ClassID = DataAccessLayer.clsClassesData.AddNewClass(this.CoachID, this.className, this.Description, this.StartTime,
+                this.EndTime, this.Note,
+                this.Maximum_Capacity, this.IsActive, this.StartDay, this.EndDay);
+
+            return (this.ClassID != -1);
+        }
+        private bool _UpdateClass()
+        {
+            //call DataAccess Layer 
+
+            return clsClassesData.UpdateClass(this.ClassID,this.CoachID,this.className,this.Description,this.StartTime,this.EndTime
+                ,this.Note,this.Maximum_Capacity,this.IsActive,this.StartDay,this.EndDay);
+        }
+
+        //Added a public `Save()` method to manage saving logic based on
+        //the current mode, supporting both adding and updating classes.
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+
+                case enMode.AddNew:
+                    if (_AddClass())
+                    {
+                        _Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdateClass();
+
+            }
+
+            return false;
+        }
+
+
+        public static DataTable GetAllClasses()
+        {
+            return clsClassesData.GetAllClasses();
+        }
+
+        public static bool DeleteClass(int ClassID)
+        {
+            return clsClassesData.DeleteClass(ClassID);
+        }
+
+        public static clsClasses GetClassInfoByClassID(int ClassID)
+        {
+            int coachID = -1;
+            byte maximumCapacity = 0;
+            string note = string.Empty;
+            string className = string.Empty;
+            string Description = string.Empty;
+            TimeSpan startTime=TimeSpan.Zero;
+            TimeSpan endTime=TimeSpan.Zero;
+            string startDay = string.Empty;
+            string endDay = string.Empty;
+            bool isActive = false;
+
+            if(clsClassesData.GetClassInfoByClassID(ClassID,ref  coachID,ref className,ref Description,ref startTime
+                ,ref endTime,ref note,ref maximumCapacity,ref isActive,ref startDay,ref endDay))
+            {
+
+                return new clsClasses(ClassID,className,Description,startTime,
+                    endTime,note,maximumCapacity,isActive,startDay,endDay,coachID);
+
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+
+
+
+    }
+}
