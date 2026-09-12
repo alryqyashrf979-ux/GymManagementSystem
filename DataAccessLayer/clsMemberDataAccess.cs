46e2a9c402a1f354b149f3420aa2b805f6a2d2d1
Ashraf <alryqyashrf979@gmail.com>
2026-09-12 18:10:22 +0300

    Added a method to find member By member Id

--- DataAccessLayer/clsMemberDataAccess.cs
+++ DataAccessLayer/clsMemberDataAccess.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    internal class clsMemberDataAccess
+    static public  class clsMemberDataAccess
     {
+        static public bool FindmemberByMemberID(int MemberID, ref int PersonID, ref DateTime LastSubscriptionDate, ref bool IsActive, ref int ContactPersonInfo)
+        {
+            string Query = "select * from Members where MemberID = @MemberID";
+            using (SqlConnection con = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(Query, con))
+            {
+                cmd.Parameters.AddWithValue("@MemberID", MemberID);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+
+                    if (reader.Read())
+                    {
+                        PersonID = (int)reader["PersonID"];
+                        LastSubscriptionDate = (DateTime)reader["LastSubscriptionDate"];
+                        IsActive = (bool)reader["IsActive"];
+                        ContactPersonInfo = (int)reader["ContactPersonInfo"];
+                        return true;
+                    }
+
+
+
+                }
+            }
+            return false;
+        }
     }
 }
