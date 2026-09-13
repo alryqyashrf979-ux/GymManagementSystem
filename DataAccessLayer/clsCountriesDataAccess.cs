8e208aee49023deb8c80c48ac8e92de54d40f3b9
Ashraf <alryqyashrf979@gmail.com>
2026-09-14 01:14:06 +0300

    added a method that returns a data table of all countries

--- DataAccessLayer/clsCountriesDataAccess.cs
+++ DataAccessLayer/clsCountriesDataAccess.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    internal class clsCountriesDataAccess
+    static public class clsCountriesDataAccess
     {
+        static public DataTable GetAllCountries()
+        {
+            DataTable dt = new DataTable();
+            string query = "select * from Countries ";
+            using (SqlConnection conn = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        dt.Load(reader);
+                    }
+
+                }
+                return dt;
+
+            }
+        }
     }
 }
