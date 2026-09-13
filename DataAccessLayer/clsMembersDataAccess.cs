0748d04e274985756a19eadb2641a1cedf9151d9
Ashraf <alryqyashrf979@gmail.com>
2026-09-13 11:47:24 +0300

    added a new class member class and added a method to get all members data from db

---
+++ DataAccessLayer/clsMembersDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class clsMembersDataAccess
+    {
+        static public DataTable GetAllMembers()
+        {
+            DataTable dt = new DataTable();
+            string Query = "select * from Members ";
+            using (SqlConnection con = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand(Query, con))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                        dt.Load(reader);
+                }
+                return dt;
+
+            }
+        }
+    }
+}
