a3fc032508fa6d4d4bb71072d649f11ddb49fc7a
Aiman Ameen <aymnamynalamry@gmail.com>
2026-09-14 21:22:16 +0200

    Make clsUsersData public and add AddNewUser method

--- DataAccessLayer/clsUsersData.cs
+++ DataAccessLayer/clsUsersData.cs
@@ -1,12 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    internal class clsUsersData
+    public static class clsUsersData
     {
+
+
+
+
+        public static int AddNewUser(int PersonID, string UserName,
+             string Password,sbyte Permission, bool IsActive)
+        {
+
+            string query = @"INSERT INTO Users (PersonID,UserName,Password,IsActive,Permission)
+                             VALUES (@PersonID, @UserName,@Password,@IsActive,@Permission);
+                             SELECT SCOPE_IDENTITY();";
+
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DataAccessSettings.ConnectionString))
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+
+
+                    conn.Open();
+
+                    command.Parameters.AddWithValue("@Permission", Permission);
+                    command.Parameters.AddWithValue("@PersonID", PersonID);
+                    command.Parameters.AddWithValue("@UserName", UserName);
+                    command.Parameters.AddWithValue("@Password", Password);
+                    command.Parameters.AddWithValue("@IsActive", IsActive);
+
+
+                    object result = command.ExecuteScalar();
+
+                    if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                    {
+                        return insertedID;
+                    }
+                    else
+                        return -1;
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return -1;
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
