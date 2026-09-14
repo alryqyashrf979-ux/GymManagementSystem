1eea05f6c1da4bcdf8cec7c3f58df55445674009
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-14 23:52:50 +0300

    GetAllEmployeeAttendenceData

--- DataAccessLayer/clsEmployeeAttendance.cs
+++ DataAccessLayer/clsEmployeeAttendance.cs
@@ -8,5 +8,36 @@ namespace DataAccessLayer
 {
     class clsEmployeeAttendance
     {
+        static public DataTable GetAllEmployeeAttendenceData()
+        {
+            DataTable dt = new DataTable();
+
+
+            SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString);
+            string query = "select AttendanceID as 'Attendance ID' , EmployeeID as 'Employee ID',IsCheckin as 'Is Checkin'," +
+                "IsCheckout as 'Is Checkout',Note,Date from EmployeesAttendance;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    dt.Load(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return dt;
+        }
+
+
     }
 }
