2a5b402c113ba551e91b33134bd91fc74a2b90d4
Ashraf <alryqyashrf979@gmail.com>
2026-09-15 00:24:30 +0300

    added all properties for the class member

--- DataBusinessLayer/clsMembers.cs
+++ DataBusinessLayer/clsMembers.cs
@@ -8,5 +8,16 @@ namespace DataBusinessLayer
 {
      public class clsMembers : clsPeople 
     {
+        private int _MemberID;
+        public int MemberID { get { return _MemberID; } }
+
+        public int personID { set; get; }
+        public DateTime LastSubscriptionID { set; get; }
+        public bool IsActive { set; get; } 
+        public int ContactPersonInfoID { set; get; }
+
+        clsEmergencyContacts EmergencyContactInfo { set; get; }
+
+
     }
 }
