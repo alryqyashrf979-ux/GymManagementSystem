78d6e8051ef345249e0ab2ad64c90be5832a9f03
Ashraf <alryqyashrf979@gmail.com>
2026-09-15 00:38:25 +0300

    added enum to set mode of the object as well as added an empty and a parameterized constructors

--- DataBusinessLayer/clsMembers.cs
+++ DataBusinessLayer/clsMembers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace DataBusinessLayer
 {
      public class clsMembers : clsPeople 
     {
+        enum enMode { add =1 , edit = 2}
+        enMode Mode = enMode.add;
         private int _MemberID;
         public int MemberID { get { return _MemberID; } }
 
@@ -17,6 +20,36 @@ namespace DataBusinessLayer
         public int ContactPersonInfoID { set; get; }
 
         clsEmergencyContacts EmergencyContactInfo { set; get; }
+
+        public clsMembers():base(){
+
+            _MemberID = -1;
+            personID = -1;
+            LastSubscriptionID = default(DateTime);
+IsActive = false;
+            ContactPersonInfoID = -1;
+            EmergencyContactInfo = null;
+            Mode = enMode.add;
+        }
+
+        public clsMembers(int MemberID ,int personID , DateTime lastSubscriptionDate , bool IsActive , int ContactPersonInfoID ,int PersonID, string FirstName, string SecondName,
+            string LastName, string NationalNo, DateTime DateOfBirth, char Gender,
+             string Address, string Phone, string Email,
+            int NationalityCountryID, string ImagePath) : base( PersonID, FirstName,  SecondName,
+           LastName,  NationalNo,  DateOfBirth, Gender,
+            Address,  Phone,  Email,
+             NationalityCountryID,  ImagePath)
+        {
+
+           this. _MemberID = MemberID;
+            this.personID = personID;
+           this. LastSubscriptionID = lastSubscriptionDate;
+           this. IsActive = IsActive;
+           this. ContactPersonInfoID = ContactPersonInfoID;
+            this.EmergencyContactInfo = clsEmergencyContacts.Find(ContactPersonInfoID);
+            Mode = enMode.edit;
+        }
+
 
 
     }
