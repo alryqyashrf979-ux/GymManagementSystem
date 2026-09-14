a6aafdc0c373e04c28664927d8df0cd9b80584cc
Aiman Ameen <aymnamynalamry@gmail.com>
2026-09-14 19:30:13 +0200

    Make clsEmergencyContacts public and add DB insert method

--- DataAccessLayer/clsEmergencyContacts.cs
+++ DataAccessLayer/clsEmergencyContacts.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    internal class clsEmergencyContacts
+    public static class clsEmergencyContacts
     {
+
+
+
+        public static int AddEmergencyContact(string Name, string Relationship, string PhoneNumber)
+        {
+            string query = "INSERT INTO EmergencyContacts (Name, Relationship, PhoneNumber) VALUES (@Name, @Relationship, @PhoneNumber);" +
+                " SELECT SCOPE_IDENTITY();";
+            try
+            {
+
+                using (SqlConnection conn = new SqlConnection(DataAccessSettings.ConnectionString))
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+
+                    conn.Open();
+
+
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    cmd.Parameters.AddWithValue("@Relationship", Relationship);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", PhoneNumber);
+
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null && int.TryParse(result.ToString(), out int newId))
+                    {
+                        return newId;
+                    }
+                    else
+                    {
+                        return -1; // Indicate failure to retrieve new ID
+                    }
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                return -1;
+            }
+        }
+
+      
+
+
+
+
+
     }
 }
