0f66edcf508b5d05f98be867a1d62fe53ce6466f
Aiman Ameen <aymnamynalamry@gmail.com>
2026-09-14 19:38:32 +0200

    Add clsEmergencyContacts class with properties and methods

--- DataBusinessLayer/clsEmergencyContacts.cs
+++ DataBusinessLayer/clsEmergencyContacts.cs
@@ -9,6 +9,40 @@ namespace DataBusinessLayer
     public class clsEmergencyContacts
     {
 
+        public string Name { get; set; }
+        public string Relationship { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private int int_ContactID = -1;
+        public int ContactID
+        {
+            get { return int_ContactID; }
+        }
+
+        public enum enMode { Update=1,AddNew=2}
+        public enMode Mode = enMode.AddNew;
+
+
+        private clsEmergencyContacts(string name, string relationship, string phoneNumber, int contactID)
+        {
+            Name = name;
+            Relationship = relationship;
+            PhoneNumber = phoneNumber;
+            int_ContactID = contactID;
+        }
+
+        public clsEmergencyContacts()
+        {
+            Name = string.Empty;
+            Relationship = string.Empty;
+            PhoneNumber = string.Empty;
+            int_ContactID = -1;
+        }
+
+
+
+
+
 
 
 
