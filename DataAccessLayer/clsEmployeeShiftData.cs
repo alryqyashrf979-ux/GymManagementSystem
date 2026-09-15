63a0bbc535e13e0c3974b4a6a4377f178db72638
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-15 23:43:08 +0300

    GetAllEmployee Shift -DataAccess

--- DataAccessLayer/clsEmployeeShiftData.cs
+++ DataAccessLayer/clsEmployeeShiftData.cs
@@ -12,5 +12,24 @@ namespace DataAccessLayer
    static public class clsEmployeeShiftData
     {
 
+        static public DataTable GetAllEmployeeShift()
+        {
+            DataTable dt = new DataTable();
+            string query = "select ShiftID as 'Shift ID',ShiftType as 'Shift Type',StartTime as 'Start Time',EndTime as 'End Time' from EmployeesShift;";
+
+            using(SqlConnection connection=new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                connection.Open();
+                using(SqlCommand command=new SqlCommand(query, connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                            dt.Load(reader);
+                    }
+                }
+            }
+            return dt;
+        }
     }
 }
