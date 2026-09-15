e34e1a924349e234163d4ea3a4eaf9d565036b64
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-15 18:01:23 +0300

    Add Data Member for Employees -Bussnise

--- DataBusinessLayer/clsEmployees.cs
+++ DataBusinessLayer/clsEmployees.cs
@@ -7,8 +7,25 @@ using System.Data;
 
 namespace DataBusinessLayer
 {
-   public class clsEmployees
+    public class clsEmployees
     {
+        private int _EmployeeID;
+        public int EmployeeID { get; }
+        public int PersonID { get; set; }
+        public int ShiftID { set; get; }
+        public string Title { get; set; }
+        public float Salary { get; set; }
+        public DateTime HiredDate { set; get; }
+        public DateTime TerminationDate { set; get; }
+        public string Notes { set; get; }
+        public bool IsActive { set; get; }
 
+        private object _Person;
+        public object Person { get { return _Person; } }
+        private object _Shift;
+        public object Shift { get { return _Shift; } }
+
+        public enum enMode { AddMode=1,UpdateMode=2};
+        enMode _Mode = enMode.AddMode;
     }
 }
