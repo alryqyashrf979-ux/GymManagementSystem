58ac2968d7112c845f05fbef9dfd573a4ea88792
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-15 16:48:50 +0300

    Update Employee DataAccess

--- DataAccessLayer/clsEmployeesData.cs
+++ DataAccessLayer/clsEmployeesData.cs
@@ -49,5 +49,91 @@ namespace DataAccessLayer
                 return false;
             }
         }
+
+        static public int AddEmployee(int PersonID,string Title,float Salary,DateTime HiredDate,string Notes,bool IsActive,int ShiftID)
+        {
+            string query = "INSERT INTO [dbo].[Employees]([PersonID],[Title],[Salary],[HiredDate],[Notes],[IsActive],[ShiftID])" +
+                " VALUES (@PersonID,@Title,@Salary,@HiredDate,@Notes,@IsActive,@ShiftID); Select scope_Idintity()";
+
+            try
+            {
+                using (SqlConnection connection=new SqlConnection(DataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using(SqlCommand command=new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("PersonID", PersonID);
+                        command.Parameters.AddWithValue("Title", Title);
+                        command.Parameters.AddWithValue("Salary", Salary);
+                        command.Parameters.AddWithValue("HiredDate", HiredDate);
+
+            
+                        if(string.IsNullOrEmpty(Notes)
+                        command.Parameters.AddWithValue("Notes", DBNull.Value);
+                        else
+                        command.Parameters.AddWithValue("Notes", Notes);
+
+                        command.Parameters.AddWithValue("IsActive", IsActive);
+                        command.Parameters.AddWithValue("ShiftID", ShiftID);
+
+                        object Resault = command.ExecuteScalar();
+
+                        if (int.TryParse(Resault.ToString(), out int Value))
+                            return Value;
+                        else
+                            return -1;
+                    }
+                }
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        static public bool UpdateEmployee(int EmployeeID,int PersonID,string Title,float Salary,DateTime HiredDate,string Notes,bool IsActive,int ShiftID)
+        {
+            string query = "UPDATE [dbo].[Employees] SET" +
+                " [PersonID] =@PersonID," +
+                "[Title] =@Title," +
+                "[Salary] =@Salary," +
+                "[HiredDate] = @HiredDate," +
+                "[TerminationDate] = @TerminationDate," +
+                "[Notes] = @Notes," +
+                "[IsActive] = @IsActive," +
+                "[ShiftID] = @ShiftID " +
+                "WHERE EmployeeID=@EmployeeID;";
+
+            try
+            {
+                using(SqlConnection connection=new SqlConnection(DataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using(SqlCommand command=new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("EmployeeID", EmployeeID);
+                        command.Parameters.AddWithValue("PersonID", PersonID);
+                        command.Parameters.AddWithValue("Title", Title);
+                        command.Parameters.AddWithValue("Salary", Salary);
+                        command.Parameters.AddWithValue("HiredDate", HiredDate);
+
+
+                        if (string.IsNullOrEmpty(Notes))
+                        command.Parameters.AddWithValue("Notes", DBNull.Value);
+                        else
+                            command.Parameters.AddWithValue("Notes", Notes);
+
+                        command.Parameters.AddWithValue("IsActive", IsActive);
+                        command.Parameters.AddWithValue("ShiftID", ShiftID);
+
+                        return Convert.ToInt32(command.ExecuteNonQuery()) > 0;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
