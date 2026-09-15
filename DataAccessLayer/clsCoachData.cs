81ce656ca61241124f9eef10a3ca1f5fd3023122
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-15 21:09:42 +0300

    Get All Coach -DataAccess

--- DataAccessLayer/clsCoachData.cs
+++ DataAccessLayer/clsCoachData.cs
@@ -11,5 +11,25 @@ namespace DataAccessLayer
    static public class clsCoachData
     {
       
+        static public DataTable GetAllCoach()
+        {
+            DataTable dt = new DataTable();
+            string query = "select CoachID as 'Coach ID' , EmployeeID as 'Employee ID',Speciality,Note from Coaches;";
+
+            using(SqlConnection connection=new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                connection.Open();
+                using(SqlCommand command=new SqlCommand(query, connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                            dt.Load(reader);
+                    }
+                }
+            }
+            return dt;
+        }
+
     }
 }
