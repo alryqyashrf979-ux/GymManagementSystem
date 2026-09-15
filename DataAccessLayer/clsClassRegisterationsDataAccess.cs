0a26e45c457c8ef84ec7b8c3d0f8a454101e58a6
Ashraf <alryqyashrf979@gmail.com>
2026-09-15 17:45:59 +0300

    added a method to get all class registeration info

--- DataAccessLayer/clsClassRegisterationsDataAccess.cs
+++ DataAccessLayer/clsClassRegisterationsDataAccess.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    static public  class clsClassRegisterationsDataAccess
+    static public class clsClassRegisterationsDataAccess
     {
+        static public DataTable GetAllClassRegisterationInfo()
+        {
+            DataTable dt = new DataTable();
+            string Query = "SELECT \r\n    ClassRegisterationID AS [Class Registeration ID],\r\n    P.FirstName + ' ' + P.SecondName + ' ' + P.LastName AS [Member Full Name],\r\n " +
+                "   C.ClassName AS [Class Name],\r\n    C.StartTime AS [Start Time]\r\nFROM ClassRegisteration\r\nINNER JOIN Members M\r\n    ON M.MemberID = ClassRegisteration.MemberID\r\nINNER JOIN People P\r\n " +
+                "   ON P.ID = M.PersonID\r\nINNER JOIN Classes C\r\n    ON C.ClassID = ClassRegisteration.ClassID;";
 
+            using (SqlConnection conn = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(Query, conn))
+            {
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                        dt.Load(reader);
+                }
+            }
+            return dt;
+        }
+
+  
     }
 }
