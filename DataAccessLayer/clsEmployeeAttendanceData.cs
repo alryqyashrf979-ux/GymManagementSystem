5865955492e1539a4097ece3ed91329b2c03ad31
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-15 13:08:47 +0300

    Get All Employee Attendance Data

--- DataAccessLayer/clsEmployeeAttendanceData.cs
+++ DataAccessLayer/clsEmployeeAttendanceData.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataAccessLayer
 {
   static public  class clsEmployeeAttendanceData
     {
+
+        static public DataTable GetAllEmployeeAttendenceData()
+        {
+            DataTable dt = new DataTable();
+
+
+            SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString);
+            string query = "select AttendanceID as 'Attendance ID' , EmployeeID as 'Employee ID',IsCheckin as 'Is Checkin'," +
+                "IsCheckout as 'Is Checkout',Note,Date from EmployeesAttendance;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    dt.Load(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return dt;
+        }
+
+       
+
+
     }
 }
