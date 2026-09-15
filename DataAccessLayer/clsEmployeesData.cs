dca0be32892ec27b107eef86b3a3e38a3d598239
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-15 15:59:52 +0300

    Get All Employees -DataAccess

--- DataAccessLayer/clsEmployeesData.cs
+++ DataAccessLayer/clsEmployeesData.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataAccessLayer
 {
    static public class clsEmployeesData
     {
+        static public DataTable GetAllEmployees()
+        {
+            DataTable dt = new DataTable();
+            string query = "Select *from Employees;";
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString)) 
+            using(SqlCommand command=new SqlCommand(query, connection))
+            {
+                connection.Open();
+                using(SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                        dt.Load(reader);
+                }
+                return dt;
+            }
 
+        }
     }
 }
