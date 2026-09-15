7dc3096e015c08a94a2d2af2a7c8a7e4fc499fd5
Ashraf <alryqyashrf979@gmail.com>
2026-09-16 01:23:22 +0300

    added a method to format and get all classes registerations

--- DataAccessLayer/clsClassregisterationsDataAccess.cs
+++ DataAccessLayer/clsClassregisterationsDataAccess.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace DataAccessLayer
 {
     static public  class clsClassregisterationsDataAccess
     {
+        static public DataTable GetAllClassRegisterationInfo()
+        {
+            DataTable dt = new DataTable();
+            string Query = "SELECT \r\n    ClassRegisterationID AS [Class Registeration ID],\r\n    P.FirstName + ' ' + P.SecondName + ' ' + P.LastName AS [Member Full Name],\r\n " +
+                "   C.ClassName AS [Class Name],\r\n    C.StartTime AS [Start Time]\r\nFROM ClassRegisterations\r\nINNER JOIN Members M\r\n    ON M.MemberID = ClassRegisteration.MemberID\r\nINNER JOIN People P\r\n " +
+                "   ON P.ID = M.PersonID\r\nINNER JOIN Classes C\r\n    ON C.ClassID = ClassRegisteration.ClassID;";
+
+            using (SqlConnection conn = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(Query, conn))
+            {
+                conn.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                        dt.Load(reader);
+                }
+            }
+            return dt;
+        }
     }
 }
