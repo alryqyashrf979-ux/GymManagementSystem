bb7180ef1529c7996815522dcb445636895bf705
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-16 11:41:20 +0300

    add clsSubscriptionChangesData

---
+++ DataAccessLayer/clsSubscriptionChangesData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DataAccessLayer
+{
+    static public class clsSubscriptionChangesData
+    {
+
+    }
+}
