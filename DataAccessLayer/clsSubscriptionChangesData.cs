880951335abafef2a479f13168ff22826ad3739d
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-16 19:47:15 +0300

    DeleteSubscriptionChanges -DataAccess

--- DataAccessLayer/clsSubscriptionChangesData.cs
+++ DataAccessLayer/clsSubscriptionChangesData.cs
@@ -114,5 +114,31 @@ namespace DataAccessLayer
             }
             return false;
         }
+    
+        static public bool DeleteSubscriptionChanges(int SubscriptionChangeID)
+        {
+            string query = "Delete From [SubscriptionChanges] where SubscriptionChangeID=@SubscriptionChangeID;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@SubscriptionChangeID", SubscriptionChangeID);
+
+                        return command.ExecuteNonQuery() > 0;
+
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return false;
+        }
+    
     }
 }
