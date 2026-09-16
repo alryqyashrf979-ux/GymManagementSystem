f168365d3c8bc384a04ecc780028fc544f9faa14
Aiman Ameen <aymnamynalamry@gmail.com>
2026-09-17 00:20:46 +0200

    Fix and enhance SQL query in FilterClassRegisterationByClassID

--- DataAccessLayer/clsClassregisterationsDataAccess.cs
+++ DataAccessLayer/clsClassregisterationsDataAccess.cs
@@ -138,15 +138,19 @@ namespace DataAccessLayer
         static public DataTable FilterClassRegisterationByClassID(int ClassID)
         {
             DataTable dt = new DataTable();
-            string Query = "SELECT \r\n    ClassRegisterationID CR AS [Class Registeration ID],\r\n    P.FirstName + ' ' + P.SecondName + ' ' + P.LastName AS [Member Full Name],\r\n " +
-                "   C.ClassName AS [Class Name],\r\n    C.StartTime AS [Start Time]\r\nFROM ClassRegisterations CR\r\nINNER JOIN Members M\r\n    ON M.MemberID = ClassRegisteration.MemberID\r\nINNER JOIN People P\r\n " +
-                "   ON P.ID = M.PersonID\r\nINNER JOIN Classes C\r\n    ON C.ClassID = ClassRegisteration.ClassID " +
-                "where CR.ClassID = @ClassID;";
+            string Query = @"SELECT  CR.ClassRegisterationID  AS [Class Registeration ID],    P.FirstName + ' ' + P.SecondName + ' ' + P.LastName AS [Member Full Name],
+                 C.ClassName AS [Class Name],    C.StartTime AS [Start Time],CR.RegisterationDate as 'Registeration Date' FROM ClassRegisterations CR INNER JOIN Members M  ON M.MemberID 
+                 = CR.MemberID INNER JOIN People P
+                   ON P.ID = M.PersonID INNER JOIN Classes C    ON C.ClassID = CR.ClassID 
+                where CR.ClassID =@ClassID";
 
             using (SqlConnection conn = new SqlConnection(DataAccessSettings.ConnectionString))
             using (SqlCommand cmd = new SqlCommand(Query, conn))
             {
                 conn.Open();
+
+                cmd.Parameters.AddWithValue("@ClassID", ClassID);
+
 
                 using (SqlDataReader reader = cmd.ExecuteReader())
                 {
