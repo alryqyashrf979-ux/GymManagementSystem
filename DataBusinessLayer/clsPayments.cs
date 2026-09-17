d3d70eac3ab085df1484feb2e0b940664622792c
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-17 17:02:25 +0300

    Add Data Member -Bussnise

--- DataBusinessLayer/clsPayments.cs
+++ DataBusinessLayer/clsPayments.cs
@@ -10,6 +10,18 @@ namespace DataBusinessLayer
 {
    public class clsPayments
     {
+        private int _PaymentID;
+        public int PaymentID { get { return _PaymentID; } }
+        public int SubscriptionID { set; get; }
+        public double PaymentAmount { set; get; }
+        public double ActualAmount { set; get; }
+        public double TotalRemaining { set; get; }
+        public int PaymentMethod { set; get; }
+        public DateTime PaymentDate { set; get; }
+        public int CreatedByUserID { set; get; }
+        public enum enMode { AddMode=1,UpdateMode=2 }
+        private enMode _Mode = enMode.AddMode;
+
 
     }
 }
