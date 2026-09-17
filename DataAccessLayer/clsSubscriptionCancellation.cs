67de9bc9ab198aff31c0a05e01b64d2f497d652d
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-17 19:25:52 +0300

    GetAllSubscriptionCancellation -DataAccess

--- DataAccessLayer/clsSubscriptionCancellation.cs
+++ DataAccessLayer/clsSubscriptionCancellation.cs
@@ -11,6 +11,27 @@ namespace DataAccessLayer
 {
    static public class clsSubscriptionCancellation
     {
-       
+       static public DataTable GetAllSubscriptionCancellation()
+        {
+            DataTable dt = new DataTable();
+
+            string query = " select SubscriptionCancellationID as 'Cancellation ID',SubscriptionID as 'Subscription ID',RefundAmount as 'Refund'," +
+                "ElapsedDays as 'Elapsed Days',CancellationDate as 'Date',Users.Username as 'Username' from SubscriptionCancellations inner join Users" +
+                " on Users.UserID=SubscriptionCancellations.CancelledByUSerID;";
+
+            using(SqlConnection connection=new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                connection.Open();
+                using(SqlCommand command=new SqlCommand(query, connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                            dt.Load(reader);
+                    }
+                }
+            }
+            return dt;
+        }
     }
 }
