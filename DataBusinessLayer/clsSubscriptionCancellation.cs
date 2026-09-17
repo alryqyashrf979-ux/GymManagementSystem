f9f5d3ba8e30f28720a985a174a2322fbfe6c9fc
Mohammed Noman <mohammedabdullahnomanqaid@gmail.com>
2026-09-17 20:56:22 +0300

    Add Data Member for clsSubscriptionCancellation -Bussnise

--- DataBusinessLayer/clsSubscriptionCancellation.cs
+++ DataBusinessLayer/clsSubscriptionCancellation.cs
@@ -10,6 +10,18 @@ namespace DataBusinessLayer
 {
    public class clsSubscriptionCancellation
     {
+        private int _CancellationID;
+        public int CancellationID { get { return _CancellationID; } }
+        public int SubscriptionID { get; set; }
+        public int CancelledByUserID { get; set; }
+        public byte ElapsedDays { get; set; }
+        public DateTime CancellationDate { get; set; }
+        public string CancellationReason { get; set; }
+        public double Refund { get; set; }
+
+        public enum enMode { AddMode=1,UpdateMode=2}
+        private enMode _Mode = enMode.AddMode;
+
 
     }
 }
